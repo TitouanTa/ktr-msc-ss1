7ff8f00862db6522f8c0ff103d02a7192329c3ad
Titouan Taillandier <Titouan.Ta@hotmail.fr>
2021-05-18 10:25:58 +0200

    added README.md

---
+++ source/repos/ktr_msc_ss1_titouan_taillandier/ktr_msc_ss1_titouan_taillandier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktr_msc_ss1_titouan_taillandier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
