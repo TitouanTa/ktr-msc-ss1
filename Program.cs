03a3033d1aea6b0a37e14eaf098aadacaba79458
Titouan Taillandier <Titouan.Ta@hotmail.fr>
2021-05-18 12:07:46 +0200

    Creating CHARACTER classe

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ktr_msc_ss1.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,10 @@ namespace ktr_msc_ss1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Character character = new Character();
+            character.Attack("RRRRrrr ....");
         }
     }
 }
